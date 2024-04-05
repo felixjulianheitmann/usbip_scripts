6b479b9665c91eb62bdbf107215e5c85fac9178e
Felix Bruns <felix@bruns.hamburg>
2024-04-05 21:38:05 +0200

    Attaching works

--- autobind.py
+++ autobind.py
@@ -3,7 +3,6 @@ import click
 from loguru import logger as log
 import asyncio
 import re
-from pathlib import Path
 
 def call(cmd):
     return run(cmd, capture_output=True).stdout.decode()
@@ -14,9 +13,7 @@ def get_ids(text):
 
 
 def get_buses(text):
-    # TODO: Figure out how to exclude matches with leading/trailing slash
-    
-    return re.findall("([0-9]+-[0-9]+\.?[0-9]?)", text)
+    return re.findall("[^/]([0-9]+-[0-9]+\.?[0-9]?):", text)
 
 
 def get_ports(text):
@@ -49,21 +46,18 @@ def list_exported():
 
 
 def attach(host, device):
-    run(["usbip", "attach", "-r", host, "-b", device])
+    return run(["usbip", "attach", "-r", host, "-b", device]).returncode == 0
 
 
 def bind(device):
-    if run(["usbip", "bind", "-b", device]).returncode == 0:
-        return True
-    else:
-        return False
+    return run(["usbip", "bind", "-b", device]).returncode == 0
 
 
 def process_device(device, host):
     if host is not None:
-        success = bind(device)
-    else:
         success = attach(host, device)
+    else:
+        success = bind(device)
 
     if success:
         log.info(f"Successfully processed [{device}]!")
@@ -83,7 +77,7 @@ def process_devices(devices, host):
 
 
 async def loop(devices, host):
-    log.info("Starting loop, binding following devices if available: ", devices)
+    log.info(f"Starting loop, binding following devices if available: {devices}")
 
     while True:
         process_devices(devices, host)
@@ -94,7 +88,7 @@ async def loop(devices, host):
 @click.command()
 @click.argument("devices_file", default="devices.txt")
 @click.argument("log_file", default="log.txt")
-@click.option("host", default=None)
+@click.argument("host", default=None)
 def main(devices_file, log_file, host):
     print("Creating log at: ", log_file)
     log.add(log_file, rotation="10 kB", retention="10 days")
